80cd2fd31277b75e067750522a285ad971721a66
steeven_js <jacques.steeven@gmail.com>
2024-07-09 11:44:17 -0400

    chore: Update package name in Android files to match new project structure

--- android/app/src/main/java/com/plantmed/MainActivity.kt
+++ android/app/src/main/java/com/plantmed/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.jsprod.plantmed
+package com.jsprod.android.plantmed
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/plantmed/MainApplication.kt
+++ android/app/src/main/java/com/plantmed/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.jsprod.plantmed
+package com.jsprod.android.plantmed
 
 import android.app.Application
 import com.facebook.react.PackageList
